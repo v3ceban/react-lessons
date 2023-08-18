e38c83fde01abda41aefecbe326b289fac428fc0
Vladimir <v3ceban@gmail.com>
2023-08-18 14:18:07 -0700

    Lesson 1

---
+++ Lesson 1/js/main.js
@@ -0,0 +1,16 @@
+var root = document.querySelector("#root");
+
+ReactDOM.render(
+	<div>
+		<h1>Lorem ipsum dolor sit amet.</h1>
+		<h2>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, qui.
+		</h2>
+		<ul>
+			<li>Lorem, ipsum dolor.</li>
+			<li>Veniam, enim. Ea?</li>
+			<li>Expedita, magnam architecto.</li>
+		</ul>
+	</div>,
+	root
+);
