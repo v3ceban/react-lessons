8dabff9282fe90dfbd59f07bfdd3313b9e1c5b83
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-02-11 14:13:06 -0800

    upd

--- old/lesson8/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./scss/main.scss";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-
-root.render(<App />);
