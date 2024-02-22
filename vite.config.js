a5523ad0f0e47b249a3093ede4ac1a2c94c1722f
Vladimir Ceban <v3ceban@proton.me>
2024-02-22 11:26:54 -0800

    build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import pages from "./pages.json";
+
+const toBuild = {};
+pages.forEach((page) => {
+  //eslint-disable-next-line no-undef
+  toBuild[page] = resolve(__dirname, `pages/${page}/index.html`);
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        //eslint-disable-next-line no-undef
+        main: resolve(__dirname, "index.html"),
+        ...toBuild,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
