4de3e351e16f7a43cf6be96667573a4b4e9c6295
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-03-23 16:51:19 -0700

    updates

--- index.jsx
+++ index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app.jsx";
+import pages from "./pages.json";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <>
+      {pages.map((page, index) => (
+        <li key={index}>
+          <a href={`/pages/${page}/`}>{page}</a>
+        </li>
+      ))}
+    </>
   </React.StrictMode>,
 );
